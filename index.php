4c36de8c56d55a4ab885ac56cd6ddc60c9dc09b5
Marko <markokaartinen@gmail.com>
2012-05-24 21:32:34 +0300

    small changes

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
 		</div>
 	
 		<div class="container">
-			<h1>Bootstrap starter template</h1>
-			<p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+			<h1>Header</h1>
+			<p>Photo folder list.</p>
 		</div> <!-- /container -->
 	
 		<!-- Le javascript
